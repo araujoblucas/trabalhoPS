a48cd1b191893ee1ff8a46c889f3c7d94fad12d5
thalialongaray <56452075+thalialongaray@users.noreply.github.com>
2021-11-19 03:24:13 -0300

    Códigos-Teste Projeto Final

---
+++ Z808/teste1.asm
@@ -0,0 +1,12 @@
+public A
+read A
+add A
+mul AX
+sub B
+jz fim
+add DX
+store AX, D
+write D
+fim hlt
+A equ 00
+B equ 99
---
+++ Z808/teste2.asm
@@ -0,0 +1,13 @@
+public D
+extrn A
+read A
+add A
+mul AX
+sub B
+jz fim
+add DX
+store AX, D
+write D
+fim hlt
+B equ 99
+D equ 10
